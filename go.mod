84f3c3a8d740cb8783c94b6691e2f67c213b22c0
v1sc0 <162381769+v1sc0@users.noreply.github.com>
2024-11-22 12:33:32 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/v1sc0/stopemailspoofing
+
+go 1.23.1
+
+require (
+	github.com/AfterShip/email-verifier v1.4.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/hbollon/go-edlib v1.6.0 // indirect
+	github.com/jpillora/go-tld v1.2.1 // indirect
+	github.com/likexian/gokit v0.25.15 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/miekg/dns v1.1.62 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+)
---
+++ modules/dkim.go
@@ -0,0 +1,79 @@
+// modules/dkim.go
+package modules
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/miekg/dns"
+)
+
+type DKIMResult struct {
+	Domain     string
+	Selector   string
+	DKIMFound  string
+	DKIMRecord string
+}
+
+var defaultSelectors = []string{
+	"default", "dkim", "selector1", "selector2", "s1", "s2", "api", "smtp", "smtpapi", "mail", "m1", "m2", "ms", "mx", "k1", "k2", "google",
+	"amazonses", "mandrill", "sparkpost", "mailjet", "sendgrid", "zoho", "office365", "mx", "o365", "postmark", "spf",
+	"transmail", "mailgun", "sendinblue", "sendpulse", "campaignmonitor", "constantcontact", "sfmc", "oracle", "smtp1",
+	"smtp2", "dmarc", "a1", "b1", "sendy", "gsuite", "gsmtp", "outlook", "smtp-relay", "domainkey", "mta", "cisco",
+	"mailchimp", "hubspot", "dynect", "ses", "alt1", "alt2", "mta-sts", "notify", "primary", "backup", "campaign",
+	"em1", "em2", "feedback", "relay", "route", "transactional", "info", "support", "newsletter", "alerts", "bulletin",
+	"webmail", "no-reply", "service", "marketing", "prod", "app", "corp", "a", "b", "c", "d", "send", "user1", "user2",
+	"user3", "public", "private", "group1", "group2", "group3", "noreply", "admin", "engage", "engagement", "portal",
+	"reports", "events", "alerts1", "alerts2", "feedback1", "feedback2", "analytics", "zimbra",
+}
+
+func ValidarDominio(dominio string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9.-]+$`)
+	return re.MatchString(dominio)
+}
+
+func VerificarDKIM(dominios []string) []DKIMResult {
+	var resultados []DKIMResult
+
+	for _, dominio := range dominios {
+		if !ValidarDominio(dominio) {
+			resultados = append(resultados, DKIMResult{Domain: dominio, DKIMFound: "Domínio inválido"})
+			continue
+		}
+
+		dkimEncontrado := false
+
+		for _, seletor := range defaultSelectors {
+			dkimQuery := fmt.Sprintf("%s._domainkey.%s", seletor, dominio)
+			resultado := DKIMResult{Domain: dominio, Selector: seletor, DKIMFound: "NÃO"}
+
+			m := new(dns.Msg)
+			m.SetQuestion(dkimQuery+".", dns.TypeTXT)
+			in, err := dns.Exchange(m, "1.1.1.1:53")
+
+			if err != nil || len(in.Answer) == 0 {
+				resultado.DKIMFound = "Sem registros TXT"
+			} else {
+				for _, ans := range in.Answer {
+					if txt, ok := ans.(*dns.TXT); ok {
+						resultado.DKIMFound = "SIM"
+						resultado.DKIMRecord = fmt.Sprintf("%s", txt.Txt)
+						dkimEncontrado = true
+						break
+					}
+				}
+			}
+
+			if dkimEncontrado {
+				resultados = append(resultados, resultado)
+				break
+			}
+		}
+
+		if !dkimEncontrado {
+			resultados = append(resultados, DKIMResult{Domain: dominio, DKIMFound: "DKIM não encontrado"})
+		}
+	}
+
+	return resultados
+}
